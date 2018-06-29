70e110d202b2d0b8421bf9ab58f03a2236acddf1
Keiho Sakapon <git@saka-pon.net>
2018-06-29 11:15:49 +0900

    Add classes for hub

---
+++ GeoSignal/GeoSignalWeb/GeoHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace GeoSignalWeb
+{
+    public class GeoHub : Hub
+    {
+        public void UpdatePosition(string id, double lat, double lng)
+        {
+            Clients.Others.NotifyPosition(id, lat, lng);
+        }
+    }
+}
